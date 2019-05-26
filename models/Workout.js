18e7b508c7b47878263e37b0432ab7e9ed2c67d4
Alex Ross <arossg17@gmail.com>
2019-05-26 13:16:39 -0400

    updated table

--- models/Workout.js
+++ models/Workout.js
@@ -3,12 +3,15 @@ const Schema = mongoose.Schema;
 
 //create book Schema and model
 const workoutSchema = new Schema ({
+    year: {type: String, required:true},
     month: {type: String, required:true},
     day: {type: String, required:true},
     timeOfDay: {type: String, required:true},
     distance: {type: String, required:true},
     calories: {type: String, required:true},
-    strides: {type: String, required:true}
+    strides: {type: String, required:true},
+    spm: {type: String, required:true},
+    time: {type: String, required:true},
 });
 
 const Workout = mongoose.model('workout', workoutSchema);
